cb9b9dcd8ca1edfe0fe27a5604a2c626babb2cd8
Keerthi285820 <srik2301@gmail.com>
2025-06-27 22:42:36 +0530

    Update models.py

--- models.py
+++ models.py
@@ -5,6 +5,15 @@ db = SQLAlchemy()
 class MovieReview(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     movie_title = db.Column(db.String(100), nullable=False)
-    reviewer = db.Column(db.String(100), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-    review = db.Column(db.Text, nullable=False)
+    reviewer    = db.Column(db.String(50), nullable=False)
+    rating      = db.Column(db.Float, nullable=False)
+    review      = db.Column(db.Text, nullable=False)
+
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "movie_title": self.movie_title,
+            "reviewer": self.reviewer,
+            "rating": self.rating,
+            "review": self.review
+        }
